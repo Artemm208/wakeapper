60218f0bde837b61b293ab3114600b5ff7eb6ee0
Sergey Borovikov <sergey.borovikov@list.ru>
2018-09-24 23:02:27 +0500

    implement simple wakeup detector

--- Wakeapper/Services/WakeupDetectorService.swift
+++ Wakeapper/Services/WakeupDetectorService.swift
@@ -14,27 +14,84 @@ import Repeat
 final class WakupDetectorService {
     
     let wakeupDetected = PublishSubject<Void>()
+    let moveDetection = PublishSubject<Bool>()
     
     private let motionManager = CMMotionManager()
-    private var latestPitch: Double = 0
-    private var latestRoll: Double = 0
-    private var latestYaw: Double = 0
+    private var timer: Repeater!
+    
+    private var latestPitch: Double?
+    private var latestRoll: Double?
+    private var latestYaw: Double?
+    
+    private var latestX: Double = 0
+    private var latestY: Double = 0
+    private var latestZ: Double = 0
+    
+    private var latestRotateX: Double = 0
+    private var latestRotateY: Double = 0
+    private var latestRotateZ: Double = 0
     
     func startWakupDetection() {
-        
         motionManager.accelerometerUpdateInterval = 0.5
-        
-        motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { [weak self] deviceMotion, error in
+        setupDeviceMotion()
+        setupAccelerometerUpdates()
+        setupGyroUpdates()
+    }
+    
+    func stopWakupDetection() {
+        motionManager.stopGyroUpdates()
+        motionManager.stopDeviceMotionUpdates()
+        motionManager.stopAccelerometerUpdates()
+    }
+    
+    private func setupTimers() {
+        timer = Repeater(interval: Repeater.Interval.minutes(1), observer: { [weak self] repeater in
+            self?.wakeupDetected.onNext(())
+        })
+    }
+    
+    private func setupDeviceMotion() {
+        motionManager.startDeviceMotionUpdates(to: .main) { [weak self] deviceMotion, error in
             
-            if self?.latestPitch == deviceMotion?.attitude.pitch.rounded() ?? 0 ||
-                self?.latestYaw == deviceMotion?.attitude.yaw.rounded() ?? 0 ||
-                self?.latestRoll == deviceMotion?.attitude.roll.rounded() ?? 0 {
-                
-            }
+            let isMoveDetection = self?.latestPitch != deviceMotion?.attitude.pitch.rounded() ?? 0 ||
+                self?.latestYaw != deviceMotion?.attitude.yaw.rounded() ?? 0 ||
+                self?.latestRoll != deviceMotion?.attitude.roll.rounded() ?? 0
+            
+            self?.moveDetection.onNext(isMoveDetection)
             
             self?.latestPitch = deviceMotion?.attitude.pitch.rounded() ?? 0
             self?.latestRoll = deviceMotion?.attitude.roll.rounded() ?? 0
             self?.latestYaw = deviceMotion?.attitude.yaw.rounded() ?? 0
+        }
+    }
+    
+    private func setupAccelerometerUpdates() {
+        motionManager.startAccelerometerUpdates(to: .main) { [weak self] accelerometerUpdates, error in
+            
+            let isMoveDetection = self?.latestX != accelerometerUpdates?.acceleration.x.rounded() ?? 0 ||
+                self?.latestY != accelerometerUpdates?.acceleration.y.rounded() ?? 0 ||
+                self?.latestZ != accelerometerUpdates?.acceleration.z.rounded() ?? 0
+            
+            self?.moveDetection.onNext(isMoveDetection)
+            
+            self?.latestX = accelerometerUpdates?.acceleration.x.rounded() ?? 0
+            self?.latestY = accelerometerUpdates?.acceleration.y.rounded() ?? 0
+            self?.latestZ = accelerometerUpdates?.acceleration.z.rounded() ?? 0
+        }
+    }
+    
+    private func setupGyroUpdates() {
+        motionManager.startGyroUpdates(to: .main) { [weak self] gyroData, error in
+            
+            let isMoveDetection = self?.latestRotateX != gyroData?.rotationRate.x.rounded() ?? 0 ||
+                self?.latestRotateY != gyroData?.rotationRate.y.rounded() ?? 0 ||
+                self?.latestRotateZ != gyroData?.rotationRate.z.rounded() ?? 0
+            
+            self?.moveDetection.onNext(isMoveDetection)
+            
+            self?.latestRotateX = gyroData?.rotationRate.x.rounded() ?? 0
+            self?.latestRotateY = gyroData?.rotationRate.y.rounded() ?? 0
+            self?.latestRotateZ = gyroData?.rotationRate.z.rounded() ?? 0
         }
     }
 
--- Wakeapper/ViewController.swift
+++ Wakeapper/ViewController.swift
@@ -7,14 +7,119 @@
 //
 
 import UIKit
+import RxSwift
+import Repeat
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    private let diposeBag = DisposeBag()
+    
+    @IBOutlet weak var moveLabel: UILabel!
+    @IBOutlet weak var smallTimerLabel: UILabel!
+    @IBOutlet weak var bigTimerLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    private var bigTimer: Repeater?
+    private var smallTimer: Repeater?
+    
+    private var remainingBigSeconds = 0
+    private var remainingSmallSeconds = 0
+    private var wakupType: WakeupType = .none {
+        didSet { self.markupView() }
+    }
+    
+    let wakeUpDetectorService = WakupDetectorService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        moveLabel.text = "Moved!"
+        moveLabel.alpha = 0
+        
+        wakeUpDetectorService.startWakupDetection()
+        wakeUpDetectorService.moveDetection
+            .filter { $0 == true }
+            .subscribe(onNext: { [weak self] isMovedDetection in
+                self?.fadeAnimateLabel()
+                self?.resetSmallTimer()
+        }).disposed(by: diposeBag)
+        
+        resetBigTimer()
     }
     
+    private func resetBigTimer() {
+        remainingBigSeconds = Constants.bigTimerInterval
+        bigTimer = Repeater.every(
+            Repeater.Interval.seconds(1),
+            count: Constants.bigTimerInterval,
+            tolerance: DispatchTimeInterval.microseconds(100),
+            queue: .main, { repeater in
+                self.remainingBigSeconds -= 1
+                self.bigTimerLabel.text = "\(self.remainingBigSeconds)"
+                if self.remainingSmallSeconds == 0 &&
+                    self.remainingBigSeconds > 0 {
+                    self.wakupType = .sleep
+                }
+        })
+    }
+    
+    private func resetSmallTimer() {
+        remainingSmallSeconds = Constants.smallTimerInterval
+        smallTimer = Repeater.every(
+            Repeater.Interval.seconds(1),
+            count: Constants.smallTimerInterval,
+            tolerance: DispatchTimeInterval.microseconds(100),
+            queue: .main, { repeater in
+                self.remainingSmallSeconds -= 1
+                self.smallTimerLabel.text = "\(self.remainingSmallSeconds)"
+                if self.remainingBigSeconds == 0 &&
+                    self.remainingSmallSeconds > 0 {
+                    self.wakupType = .wakeup
+                }
+        })
+    }
+    
+    private func stopTimvers() {
+        smallTimer?.pause()
+        smallTimer = nil
+        bigTimer?.pause()
+        bigTimer = nil
+        remainingBigSeconds = 0
+        remainingSmallSeconds = 0
+        wakeUpDetectorService.stopWakupDetection()
+    }
+    
+    private func fadeAnimateLabel() {
+        self.moveLabel.alpha = 1
+        UIView.animate(withDuration: TimeInterval(Constants.smallTimerInterval)) {
+            self.moveLabel.alpha = 0
+        }
+    }
+    
+    private func markupView() {
+        switch wakupType {
+        case .none:
+            self.view.backgroundColor = .white
+        case .sleep:
+            self.view.backgroundColor = .red
+            self.infoLabel.text = "You fell asleep"
+            self.stopTimvers()
+        case .wakeup:
+            self.view.backgroundColor = .green
+            self.infoLabel.text = "You're awake!"
+            self.stopTimvers()
+        }
+    }
+    
+    struct Constants {
+        static let bigTimerInterval = 100
+        static let smallTimerInterval = 20
+    }
+    
+}
+
+enum WakeupType {
+    case none
+    case sleep
+    case wakeup
 }
